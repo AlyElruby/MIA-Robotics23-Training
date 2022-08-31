894adca73bababeab65366731228179f68fb00dd
AlyElruby <alyelroby567@gmail.com>
2022-08-31 11:09:31 +0200

    upd

--- encoderTask/encoderTask.ino
+++ encoderTask/encoderTask.ino
@@ -1,111 +1,88 @@
 #include <Event.h>
 #include <Timer.h>
-int motor1dirc1 = 9;
-int motor1dirc2 = 10;
-int motor2dirc1 = 11;
-int motor2dirc2 = 12;
-#define PA1  2
-#define PB1  3 
-#define PA2  4
-#define PB2  5
-#define sw   6
+int motor1PWM = 9;
+int motor1dirc = 7;
+int motor2PWM = 10;
+int motor2dirc = 8;
+int P_A1 = 2;
+int P_B1 = 3;
+int P_A2 = 4;
+int P_B2 = 5;
+int sw = 6;
 long long count1=0;
 long long count2=0;
-float angle1,angle2;
+float angle1,angle2,diff1,diff2;
 Timer mytime;//creating an object from class timer
 void setup() {
   Serial.begin(9600);//to use the serial monitor
-  pinMode(motor1dirc1,INPUT);
-  pinMode(motor1dirc2,INPUT);
-  pinMode(motor2dirc1,INPUT);
-  pinMode(motor2dirc2,INPUT);
-  pinMode(PA1,INPUT_PULLUP);
-  pinMode(PB1,INPUT_PULLUP);
-  pinMode(PA2,INPUT_PULLUP);
-  pinMode(PB2,INPUT_PULLUP);
+  pinMode(motor1dirc,OUTPUT);
+  pinMode(motor2dirc,OUTPUT);
+  pinMode(motor1PWM,OUTPUT);
+  pinMode(motor2PWM,OUTPUT);
+  pinMode(P_A1,INPUT_PULLUP);
+  pinMode(P_B1,INPUT_PULLUP);
+  pinMode(P_A2,INPUT_PULLUP);
+  pinMode(P_B2,INPUT_PULLUP);
   pinMode(sw,INPUT);//pull down this button on circuit
-  attachInterrupt(PA1,ISRA1,CHANGE);
-  attachInterrupt(PB1,ISRB1,CHANGE);
-  attachInterrupt(PA2,ISRA2,CHANGE);
-  attachInterrupt(PB2,ISRB2,CHANGE);
+  attachInterrupt(P_A1,ISRA1,CHANGE);
+  attachInterrupt(P_B1,ISRB1,CHANGE);
+  attachInterrupt(P_A2,ISRA2,CHANGE);
+  attachInterrupt(P_B2,ISRB2,CHANGE);
   mytime.every(10,updateAngle);
 }
 void loop() {
   if(digitalRead(sw)==0){
-    sw0();
-    sw02();
+    sw_0();
+    sw_02();
   }
    else{
-    sw0();
-    sw12();
+    sw_0();
+    sw_12();
    }
 }
 void updateAngle(){
   angle1=(count1*360)/2400;
   angle2=(count2*360)/2400;
+  Serial.print("angle1 = ");
+  Serial.print(angle1);
+  Serial.print("angle2 = ");
+  Serial.print(angle2);
 }
-void sw0(){
-  if(angle1<60){
-    digitalWrite(motor1dirc1,HIGH);
-    digitalWrite(motor1dirc2,LOW);
-  }
-  else if(angle1>60){
-    digitalWrite(motor1dirc1,LOW);
-    digitalWrite(motor1dirc2,HIGH);
-  }
-  else{
-    digitalWrite(motor1dirc1,HIGH);
-    digitalWrite(motor1dirc2,HIGH);
-  }
+void sw_0(){
+  diff1=60-angle1;
+  digitalWrite(motor1dirc,LOW);
+  analogWrite(motor1PWM,map(diff1,0,60,0,255));
 }
-void sw02(){
-  if(angle2<0){
-    digitalWrite(motor2dirc1,HIGH);
-    digitalWrite(motor2dirc2,LOW);
-  }
-  else if(angle2>0){
-    digitalWrite(motor2dirc1,LOW);
-    digitalWrite(motor2dirc2,HIGH);
-  }
-  else{
-    digitalWrite(motor2dirc1,HIGH);
-    digitalWrite(motor2dirc2,HIGH);
-  }
+void sw_02(){
+  diff2=angle2-0;
+  digitalWrite(motor2dirc,HIGH);
+  analogWrite(motor2PWM,map(diff2,0,90,0,255));
 }
-void sw12(){
-  if(angle1<90){
-    digitalWrite(motor2dirc1,HIGH);
-    digitalWrite(motor2dirc2,LOW);
-  }
-  else if(angle1>90){
-    digitalWrite(motor2dirc1,LOW);
-    digitalWrite(motor2dirc2,HIGH);
-  }
-  else{
-    digitalWrite(motor2dirc1,HIGH);
-    digitalWrite(motor2dirc2,HIGH);
-  }
+void sw_12(){
+  diff2=90-angle2;
+  digitalWrite(motor2dirc,LOW);
+  analogWrite(motor2PWM,map(diff2,0,90,0,255));
 }
 void ISRA1(){
-  if(digitalRead(PA1) !=digitalRead(PB1))
+  if(digitalRead(P_A1) !=digitalRead(P_B1))
     count1++;
    else
     count1--;
 }
 void ISRB1(){
-  if(digitalRead(PA1) ==digitalRead(PB1))
+  if(digitalRead(P_A1) ==digitalRead(P_B1))
     count1++;
    else
     count1--;
 }
 void ISRA2(){
-  if(digitalRead(PA2) !=digitalRead(PB2))
+  if(digitalRead(P_A2) !=digitalRead(P_B2))
     count2++;
    else
     count2--;
 }
 void ISRB2(){
-  if(digitalRead(PA2) ==digitalRead(PB2))
+  if(digitalRead(P_A2) ==digitalRead(P_B2))
     count2++;
    else
     count2--;
